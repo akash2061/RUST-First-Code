35755162272468733fac82815162b45e8059cac4
akash2061 <lucifermorningstar2061@gmail.com>
2024-09-09 23:15:55 +0530

    Variables, Tuples & array

--- src/recap.rs
+++ src/recap.rs
@@ -11,6 +11,33 @@ pub fn recap() {
 
     let num: i32 = i32_input();
     println!("Read Number: {}", num);
+
+    println!("\nVariables:");
+    let x = 10; // ! Mutable using mut
+    println!("x: i32 : {}", x);
+
+    const CONSTANT: i32 = 1_000; // ! Can't be mutable.
+    println!("Constant : {}", CONSTANT);
+
+    let x: u32 = 20; // ! Shadowing
+    println!("Shadowing of x: u32 : {}", x);
+
+    println!("\nTuples:");
+    let tup = ("Morningstar", 2061);
+    let (name, _year) = tup;
+    let year = tup.1;
+
+    println!("{} - {} - {}", name, year, tup.0);
+
+    println!("\nArray:");
+    let byte: [i8; 5] = [0; 5];
+    for i in 0..byte.len() {
+        print!("{:?} ", byte.get(i));
+    }
+    println!();
+    for i in byte.iter() {
+        print!("{} ", i);
+    }
 }
 
 fn string_input() -> String {
